0e0f956f2acb222d25c4dea2a27a08fc596ae87e
Jadeb21 <151123659+Jadeb21@users.noreply.github.com>
2025-10-23 12:20:21 +0200

    Structure

---
+++ list.h
@@ -0,0 +1,28 @@
+#ifndef __LIST_H__
+#define __LIST_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Structure de type cellule :
+typedef struct cell {
+    int sommet_arrivee;
+    float proba;
+    struct cell* suivante;
+} Cell;
+
+// Structure de type liste :
+typedef struct liste {
+    Cell* head;
+} Liste;
+
+// structure de liste adjacente
+typedef struct liste_adj{
+    Liste* tab_liste;
+    int nb_sommets;
+} ListeAdj;
+
+void add_list_cell(Liste* liste, Cell* cell);
+
+#endif
