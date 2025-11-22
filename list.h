46d70418138d62ef52d27ce5f085c1540e7ef4e0
Thomasd-p <thomas.penitot@efrei.net>
2025-11-22 12:11:51 +0100

    Structures

--- list.h
+++ list.h
@@ -5,24 +5,40 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Structure de type cellule :
+/**
+Structure d'une cellule (une cellule = une arrête d'un sommet vers un autre):
+- Numéro du sommet d'arrivée
+- Probabilité associée
+- pointeur vers la cellule suivante de la liste
+**/
 typedef struct cell {
     int sommet_arrivee;
     float proba;
     struct cell* suivante;
 } cell;
 
-// Structure de type liste :
+/**
+Structure d'une liste (représente les arrêtes sortantes d'un sommet):
+- head -> première cellule
+- tail -> dernière cellule (utile pour ajouter en fin de liste)
+**/
 typedef struct liste {
     struct cell* head;
     struct cell* tail;
 } liste;
 
-// structure de liste adjacente
+/**
+Structure d'une liste d'adjacence (représentation d'un graphe):
+- Tableau de listes
+- Nombre de sommets du graphe
+**/
 typedef struct liste_adj{
     liste* tab_liste;
     int nb_sommets;
 } listeAdj;
+
+
+//*******PROTOTYPES*******
 
 cell *createCell(int arrivee, float probab);
 liste *createListe();
