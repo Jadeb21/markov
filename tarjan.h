3347dd2a8ab35e4b3bdf8caf45bb234856cd67de
Thomasd-p <thomas.penitot@efrei.net>
2025-11-22 12:46:18 +0100

    Structures tarjan.h

--- tarjan.h
+++ tarjan.h
@@ -3,7 +3,12 @@
 
 #include "list.h"
 
-// structure pour identification de chaque sommet
+/** Structure pour représenter chaque sommet:
+- Numéro de sommet dans le graphe
+-
+-
+- Indique si le sommet est dans la pile
+**/
 typedef struct tarjan_vertex {
     int id;
     int num;
@@ -11,6 +16,12 @@ typedef struct tarjan_vertex {
     int in_pile;
 } t_tarjan_vertex;
 
+/** Structure pour représenter une classe:
+- Nom pour l'affichage
+- Tableau dynamique avec les indices des sommets
+- Nombre de sommets dans la classe
+- Capacité du tableau qui augmente au besoin
+**/
 typedef struct classe {
     char nom[10];
     int *sommets;
@@ -18,13 +29,21 @@ typedef struct classe {
     int capacite;
 } t_classe;
 
+// Structure d'une partition (= ensemble de classes):
 typedef struct partition {
     t_classe *classes;
     int taille;
     int capacite;
 } t_partition;
 
-
+/** Structure contenant toutes les informations nécessaires pour l'algorithme de Tarjan:
+- Informations sur chaque sommet
+- La pile utilisé
+- La taille actuelle de la pile
+- Numérotation
+- Le graphe
+- Le résultat
+**/
 typedef struct tarjan_data {
     t_tarjan_vertex *sommets;
     int *pile;
@@ -33,6 +52,8 @@ typedef struct tarjan_data {
     listeAdj *graphe;
     t_partition *partition;
 } t_tarjan_data;
+
+//*******PROTOTYPES*******
 
 t_tarjan_data* initialiser_tarjan(listeAdj *g);
 void tarjan_parcours(t_tarjan_data *data, int sommet_index);
